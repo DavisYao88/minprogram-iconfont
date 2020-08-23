5cd3efb5e43628eca911fe9161d01e8ce7a3bd32
黑山老妖 <ylhui@mbadashi.com>
2020-08-23 23:25:58 +0800

    测试软连接

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @Author: heishanlaoyao 
  * @Date: 2020-08-22 01:13:48 
  * @Last Modified by: heishanlaoyao
- * @Last Modified time: 2020-08-22 21:38:12
+ * @Last Modified time: 2020-08-23 23:15:02
  */
 const {readJson } = require('./lib/fileUtil');
 const { executeGeneate} = require('./command/geneateStyleFont');
@@ -11,10 +11,10 @@ const {command} = require('./lib/command');
 const { initIconfontConfig} = require('./command/initConfig')
 const { getProjectRootPath } = require('./lib/rootPath')
 const colors = require('colors');
-
 (async () => {
     let rootPath = getProjectRootPath()
     try {
+       
         let packageJson = await readJson(`${rootPath}/package.json`)
         const commands = [
             {
