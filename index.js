3d1afe973aa7e667bdc6ad289afc9e2502675235
黑山老妖 <ylhui@mbadashi.com>
2020-08-24 10:08:39 +0800

    修改配置

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @Author: heishanlaoyao 
  * @Date: 2020-08-22 01:13:48 
  * @Last Modified by: heishanlaoyao
- * @Last Modified time: 2020-08-23 23:15:02
+ * @Last Modified time: 2020-08-24 10:07:59
  */
 const {readJson } = require('./lib/fileUtil');
 const { executeGeneate} = require('./command/geneateStyleFont');
